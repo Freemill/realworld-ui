0884974b6929994dd581109a54a9ee48b993e5a8
신명상 <mongddangand@likelion.org>
2024-02-25 22:12:39 +0900

    NO-TASK baseUrl 수정

--- src/shared/api/realworld/index.ts
+++ src/shared/api/realworld/index.ts
@@ -1,5 +1,5 @@
 export function baseUrl(path: string) {
-  return `http://localhost:8080/api${path}`;
+  return `https://critical-world.duckdns.org/api${path}`;
 }
 
 export type { UnexpectedErrorDto } from './realworld.types';
